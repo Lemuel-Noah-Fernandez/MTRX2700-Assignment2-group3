36f88862a18ffa3a4234b7b432acd2fc45244e4d
eglynn214879 <102641219+eglynn214879@users.noreply.github.com>
2022-04-11 13:42:24 +1000

    Add files via upload

--- Proj2Ex1/Sources/serial.c
+++ Proj2Ex1/Sources/serial.c
@@ -11,6 +11,7 @@
 SerialPort SCI1 = {&SCI1BDH, &SCI1BDL, &SCI1CR1, &SCI1CR2, &SCI1DRL, &SCI1SR1};
 SerialPort SCI0 = {&SCI0BDH, &SCI0BDL, &SCI0CR1, &SCI0CR2, &SCI0DRL, &SCI0SR1};
 
+int counter = 0;
 
 
 // InitialiseSerial - Initialise the serial port SCI1
@@ -44,55 +45,47 @@ void SerialInitialise(int baudRate, SerialPort *serial_port) {
   *(serial_port->ControlRegister2) = SCI1CR2_RE_MASK | SCI1CR2_TE_MASK | SCI1CR2_TCIE_MASK;
   *(serial_port->ControlRegister1) = 0x00;
 }
-    
-        
-void SerialOutputChar(char data, SerialPort *serial_port) {  
-  while((*(serial_port->StatusRegister) & SCI1SR1_TDRE_MASK) == 0){
-  }
-  
-  *(serial_port->DataRegister) = data;
-}
 
 
-
-void SerialOutputString(char *pt, SerialPort *serial_port) {
-  while(*pt) {
-    SerialOutputChar(*pt, serial_port);
-    pt++;
-  }            
-}
-
-void SerialInputChar(SerialPort *serial_port, char data) {  
-  while((*(serial_port->StatusRegister) & SCI1SR1_TDRE_MASK) == 0){
-  }
-  
-  *(serial_port->DataRegister) = data;
-}
-
-
-
-void SerialInputString(SerialPort *serial_port, char *pt) {
-  while(*pt) {
-    SerialInputChar(serial_port, *pt);
-    pt++;
-  }            
-}
 
 // look at the isr_vectors.c for where this function is 
 //  added to the ISR vector table
 #pragma CODE_SEG __NEAR_SEG NON_BANKED /* Interrupt section for this module. Placement will be in NON_BANKED area. */
-__interrupt void SCI1_ISR(void) { 
-     int counter = 0;
-     char string_input[8], string_buffer[8];
-     sprintf(&string_input[0], "Bruh\n\r");
-     
-     SerialInputString(&SCI1, &string_input[0]);
-     sprintf(&string_buffer[0], &SCI1);
-     //SerialOutputString(&string_buffer[0], &SCI0);
-     SerialOutputString(&string_input[0], &SCI0);
-    
+__interrupt void SCI1_ISR(SerialPort *serial_port_in, SerialPort *serial_port_out, char *pt, char *input_string) {
+  //set counter to read for 256 bits or until end character falg 'x' is reached
+  char output_string[256];
+  while(counter < 256 && *(serial_port_in->DataRegister) != 'x'){
+    while((*(serial_port_in->StatusRegister) & SCI1SR1_RDRF_MASK) == 0){
+    }
+    //store read characters in output string and memory pointer
+    output_string[counter] = *(serial_port_in->DataRegister);
+    *pt = output_string[counter];
+    pt++;
+    counter++;
+  }
+ 
+  //output string until termination character is reached
+  counter = 0;
+  while(*input_string != 'x'){
+    while((*(serial_port_out->StatusRegister) & SCI1SR1_TDRE_MASK) == 0){
+    }
+    //transmit output string to serial port
+    *(serial_port_out->DataRegister) = *input_string;
+    *input_string++;
+    counter++;
+    //termiante string if it reaches 256 chracters
+    if(counter == 255){*input_string = 'x';}
+  }
+  //wait for final two chracters to be read
+  while((*(serial_port_out->StatusRegister) & SCI1SR1_TDRE_MASK) == 0){
+  }
+  *(serial_port_out->DataRegister) = *input_string;
+  *input_string++;
+  while((*(serial_port_out->StatusRegister) & SCI1SR1_TDRE_MASK) == 0){
+  }
+  //terminate interrupts from TCIE
+  *(SCI1.ControlRegister2) &= ~SCI1CR2_TCIE_MASK;
 }
-
 
 
 
